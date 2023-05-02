d00d815c73a5247f0f5bf05efa6a949f6adf58bd
Jon <lee.jon.scott@gmail.com>
2023-05-02 10:47:29 -0400

    feat: reworked button styles using cx for easier management

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,8 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -12,6 +12,6 @@ const preview: Preview = {
   },
 };
 
-import '../src/tailwind.css'; // replace with the name of your tailwind css file
+import '../src/tailwind.css';
 
 export default preview;
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import cx from 'classnames';
 
 export enum ButtonVariants {
   PRIMARY = 'primary',
@@ -13,39 +14,40 @@ export enum ButtonSizes {
 }
 
 type ButtonProps = {
+  active?: boolean;
   additionalClasses?: string;
   size: ButtonSizes;
   disabled?: boolean;
   variant?: ButtonVariants;
 };
 
-const rootClasses =
-  'py-3 px-4 font-medium tracking-tight uppercase transition ease-in-out disabled:opacity-50 disabled:pointer-events-none';
-
-const variantClasses = {
-  [ButtonVariants.PRIMARY]: 'button--primary',
-  [ButtonVariants.SECONDARY]: 'button--secondary ',
-  [ButtonVariants.TERTIARY]: 'button--tertiary',
-};
-
-const sizeClasses = {
-  [ButtonSizes.SMALL]: 'text-sm',
-  [ButtonSizes.MEDIUM]: 'text-md',
-  [ButtonSizes.LARGE]: 'text-base',
-};
+const rootClasses = 'button py-3 px-4 tracking-tight uppercase transition ease-in-out';
 
 export const Button: FC<React.PropsWithChildren<ButtonProps>> = ({
+  active = false,
   additionalClasses = '',
   children,
   disabled = false,
   size = ButtonSizes.LARGE,
   variant = ButtonVariants.PRIMARY,
 }) => {
+  const classes = cx(
+    rootClasses,
+    {
+      'button--primary': variant === ButtonVariants.PRIMARY,
+      'button--secondary': variant === ButtonVariants.SECONDARY,
+      'button--tertiary': variant === ButtonVariants.TERTIARY,
+      'text-sm': size === ButtonSizes.SMALL,
+      'text-md': size === ButtonSizes.MEDIUM,
+      'text-base': size === ButtonSizes.LARGE,
+      'opacity-50 pointer-events-none': disabled,
+      'button--active': active,
+    },
+    additionalClasses,
+  );
+
   return (
-    <button
-      disabled={disabled}
-      className={`${rootClasses} ${variantClasses[variant]} ${sizeClasses[size]} ${additionalClasses}`}
-    >
+    <button disabled={disabled} className={classes}>
       {children}
     </button>
   );
