babca140f1fbe80ff6b426003c89463a1cc3c31d
Jon <lee.jon.scott@gmail.com>
2023-05-04 22:01:48 -0400

    feat: added a min-width to select, so it looks nicer

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -20,7 +20,7 @@ export const Select: FC<SelectProps> = ({ items, label }) => {
             {label}
           </Listbox.Label>
           <div className="relative mt-2">
-            <Listbox.Button className="relative cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-primary-500 sm:text-sm sm:leading-6">
+            <Listbox.Button className="min-w-[175px] relative cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-primary-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
                 <span className="block truncate">{selected}</span>
               </span>
@@ -39,7 +39,7 @@ export const Select: FC<SelectProps> = ({ items, label }) => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="min-w-[175px] absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {items.map((item, i) => (
                   <Listbox.Option
                     key={`item-${i}`}
